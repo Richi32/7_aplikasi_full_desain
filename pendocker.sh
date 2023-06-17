ae5da06c8997fa8b4aa65e9d2ee89743af5b38c3
risema <richi2001.rsm@gmail.com>
2023-06-17 11:23:40 +0700

    p

--- pendocker.sh
+++ pendocker.sh
@@ -28,7 +28,7 @@ repo_name=$(basename $repo_url .git)
 
 # Clone repositori dengan nama folder yang baru
 clear
-output=$(sudo git clone $repo_url $repo_name)
+output=$(git clone $repo_url $repo_name)
 echo "$PORT_AKSES-$PORT_BACKEND-$PORT_DATABASE-$repo_name" >> list_port.txt
 cd $repo_name
 echo "Berhasil mengekstrak aplikasi dari git"
--- process_form.php
+++ process_form.php
@@ -6,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $PORT_AKSES = $_POST["PORT_AKSES"];
 
     // Menjalankan shell script untuk memproses data
-    $output = shell_exec("bash pendocker.sh '$LINK_GIT' '$NAMA_DATABASE' '$PORT_AKSES'");
+    $output = shell_exec("bash /home/richi/Desktop/meja kerja pa/7_aplikasi_full_desain/pendocker.sh '$LINK_GIT' '$NAMA_DATABASE' '$PORT_AKSES'");
 
     // Menampilkan hasil output dari shell script
     echo "<h1>Hasil Form</h1>";
